11cd364eec4749073f12d78a04f723b734643a6f
ccxxyy23 <ccxxyy23@outlook.com>
2023-11-03 12:50:38 -0700

    add route in App.js and add link to home for sidebar and headbar

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import { Routes, Route, Navigate } from "react-router";
 import SignUp from "./Components/SignUp";
 import ProfileSetting from "./Components/Profile-Setting";
 import Home from "./Home";
-import {Provider} from "react-redux";
+import HeadBar from "./Components/HeadBar";
+import SideBar from "./Components/SideBar";
+import { Provider } from "react-redux";
 import store from "./store";
 
 function App() {
   return (
-      <Provider store={store}>
-          <HashRouter>
-              <Routes>
-                  <Route path="/" element={<Home/>} />
-                  <Route path="/SignUp" element={<SignUp />} />
-                  <Route path="/profile-setting" element={<ProfileSetting/>} />
-              </Routes>
-          </HashRouter>
-      </Provider>
-
+    <Provider store={store}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/profile-setting" element={<ProfileSetting />} />
+          <Route path="/HeadBar" element={<HeadBar />} />
+          <Route path="/SideBar" element={<SideBar />} />
+        </Routes>
+      </HashRouter>
+    </Provider>
   );
 }
 
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,13 +1,23 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import HeadBar from "../Components/HeadBar";
 
-export default function Home(){
-    return(
-        <div>
-            {/* <HeadBar /> */}
-            <h1>Home Page</h1>
-            <Link to="/profile-setting"><h2>Profile Setting Page</h2></Link>
-            <Link to="/SignUp"><h2>Sign Up Page</h2></Link>
-        </div>
-    )
-};
+export default function Home() {
+  return (
+    <div>
+      {/* <HeadBar /> */}
+      <h1>Home Page</h1>
+      <Link to="/profile-setting">
+        <h2>Profile Setting Page</h2>
+      </Link>
+      <Link to="/SignUp">
+        <h2>Sign Up Page</h2>
+      </Link>
+      <Link to="/HeadBar">
+        <h2>HeadBar Page</h2>
+      </Link>
+      <Link to="/Sidebar">
+        <h2>Sidebar Page</h2>
+      </Link>
+    </div>
+  );
+}
