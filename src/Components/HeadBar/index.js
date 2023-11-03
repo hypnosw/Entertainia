ad79cce9cdddf6f02af68c9d5c92986143163c6c
fengzhihan <fengzhihan98@gmail.com>
2023-11-04 00:40:47 +0800

    headBar 1.0

---
+++ src/Components/HeadBar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+
+const HeadBar = () => {
+    return (
+        <div className="headbar">
+            <div className="d-inline-flex justify-content-between
+                        align-items-center">
+                <img src="#" alt="" className="form-control headbar-icon" />
+                <input type="text" placeholder="Anything New for today?" />
+                <button className="form-control search-btn">Search</button>
+                <div class="d-grid gap-1 d-md-flex" >
+                    <button type="button" class="btn btn-outline-primary btn-sm">Log In</button>
+                    <button type="button" class="btn btn-outline-primary btn-sm">Settings</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default HeadBar;
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,9 +1,11 @@
 import {Link} from "react-router-dom";
+import HeadBar from "../Components/HeadBar";
 
 export default function Home(){
     return(
         <div>
-            <h1>Hone Page</h1>
+            {/* <HeadBar /> */}
+            <h1>Home Page</h1>
             <Link to="/profile-setting"><h2>Profile Setting Page</h2></Link>
             <Link to="/SignUp"><h2>Sign Up Page</h2></Link>
         </div>
