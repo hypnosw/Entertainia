64cb9e529f329710d0817fbb4ddd8ecb0696a599
Kay‘s MacPro <kaysmacpro@Kaysdebijibendiannao.local>
2023-11-10 01:26:59 -0800

    sidebar updated 11/10/23

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./index.css";
+import { FaHome, FaUserFriends, FaEnvelope, FaBell, FaList } from 'react-icons/fa';
 
 const Sidebar = () => {
     return (
       <div className="sidebar">
+        <div className="shared-parent-container">
         <div className="top-section">
           <button className="post-button">POST</button>
-        </div>
+        </div></div>
         <div className="middle-section">
           <ul className="sidebar-list">
-            <li className="sidebar-item">HOME</li>
-            <li className="sidebar-item">FOLLOWING</li>
-            <li className="sidebar-item">MESSAGES</li>
-            <li className="sidebar-item">NOTIFICATIONS</li>
-            <li className="sidebar-item">LISTS</li>
+          <li className="sidebar-item"><FaHome /> HOME</li>
+          <li className="sidebar-item"><FaUserFriends /> FOLLOWING</li>
+          <li className="sidebar-item"><FaEnvelope /> MESSAGES</li>
+          <li className="sidebar-item"><FaBell /> NOTIFICATIONS</li>
+          <li className="sidebar-item"><FaList /> LISTS</li>
           </ul>
         </div>
         <div className="bottom-section">
